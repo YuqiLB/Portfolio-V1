f3f1df0d8ca5bdb0e8b4ae16348a35addc8d326e
YuqiLB <bondliuyq@gmail.com>
2025-07-26 12:22:56 -0600

    Make pages
    Navbar, About, Contact, Home

---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar
+        <img src="" alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Contacts</li>
+            <li>About</li>
+        </ul>
+        <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Navbar
